73cd85ff4e8aa1c6cdcb3700bed715e17cf643e6
Daniel hernandez <danielhernandez16@hotmail.com>
2016-08-20 10:50:54 -0500

    Create CRUD for sales and implemented tabs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,1 +1,51 @@
 var salesControllers = angular.module('salesControllers', []);
+
+salesControllers.controller('saleController', [
+  '$scope',
+  '$stateParams',
+  '$state',
+  'saleService',
+  'productService',
+  'serviceService',
+  function(
+    $scope,
+    $stateParams,
+    $state,
+    saleService,
+    productService,
+    serviceService
+  )
+  {
+	  $scope.sales = saleService.list();
+	  $scope.sale = saleService.detail({id: $stateParams.id});
+	  $scope.products = productService.list();
+	  $scope.services = serviceService.list();
+
+	  $scope.create = function () {
+	    saleService.create($scope.sale);
+	    $scope.sales = saleService.list();
+	    $state.go('tab.sale-list');
+	  }
+
+	  $scope.update = function () {
+	    saleService.update($scope.sale);
+	    $scope.sales = saleService.list();
+	    $state.go('tab.sale-list');
+	  }
+
+	  $scope.delete = function () {
+	    saleService.delete($scope.sale);
+	    $scope.sales = saleService.list();
+	    $state.go('tab.sale-list');
+	  }
+
+	  $scope.cancel = function () {
+	    $state.go('tab.sale-list');
+	  }
+
+	  $scope.$on('$stateChangeSuccess', function() {
+	    $scope.sales = saleService.list();
+	  })
+
+	}
+]);
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,1 +1,59 @@
 var salesRoutes = angular.module('salesRoutes', []);
+
+salesRoutes.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+    // Sales
+  	.state('tab.sale-list', {
+  	  url: '/sale-list',
+      views: {
+        'tab-sales': {
+          templateUrl: 'templates/sale/sale-list.html',
+          controller: 'saleController'
+        }
+      }
+  	})
+  	.state('tab.sale-detail', {
+      url: '/sale-detail/:id',
+      views: {
+        'tab-sales': {
+          templateUrl: 'templates/sale/sale-detail.html',
+          controller: 'saleController'
+        }
+      }
+    })
+    .state('tab.sale-create', {
+      url: '/sale-create',
+      views: {
+        'tab-sales': {
+          templateUrl: 'templates/sale/sale-create.html',
+          controller: 'saleController'
+        }
+      }
+    })
+    .state('tab.sale-update', {
+      url: '/sale-update/:id',
+      views: {
+        'tab-sales': {
+          templateUrl: 'templates/sale/sale-update.html',
+          controller: 'saleController'
+        }
+      }
+    })
+    .state('tab.sale-delete', {
+      url: '/sale-delete/:id',
+      views: {
+        'tab-sales': {
+          templateUrl: 'templates/sale/sale-delete.html',
+          controller: 'saleController'
+        }
+      }
+    })
+
+  $urlRouterProvider.otherwise('/tab/sale-list');
+
+}]);
